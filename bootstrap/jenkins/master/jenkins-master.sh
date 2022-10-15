14f243c2199a6ea7e7400c907c7c1afc387984ef
Belfi Gor <belfi.dev@gmail.com>
2022-10-15 13:39:40 +0500

    Add files via upload

--- bootstrap/jenkins/master/jenkins-master.sh
+++ bootstrap/jenkins/master/jenkins-master.sh
@@ -1,5 +1,8 @@
 # repo override
-sed -i 's/us.archive.ubuntu.com/mirror.hoster.kz/g' /etc/apt/sources.list
+#kz
+#sed -i 's/us.archive.ubuntu.com/mirror.hoster.kz/g' /etc/apt/sources.list
+#ru
+sed -i 's/us.archive.ubuntu.com/mirror.linux-ia64.org/g' /etc/apt/sources.list
 
 useradd $1 -s /bin/bash -d /home/test
 mkdir /home/test
