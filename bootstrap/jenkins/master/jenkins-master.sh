20cee4eada2f70a7bb8c6d0404b180c94547b023
Belfigor <belfi.dev@gmail.com>
2022-10-15 17:52:46 +0500

    test ansible playbook added test tinc vagrant vm deploy config added jenkins vagrant config improved

--- bootstrap/jenkins/master/jenkins-master.sh
+++ bootstrap/jenkins/master/jenkins-master.sh
@@ -7,7 +7,7 @@ sed -i 's/us.archive.ubuntu.com/mirror.linux-ia64.org/g' /etc/apt/sources.list
 useradd $1 -s /bin/bash -d /home/test
 mkdir /home/test
 chown -R test:test /home/test
-echo '$1    ALL=(ALL:ALL) ALL' > /etc/sudoers
+echo ''$1'    ALL=(ALL:ALL) ALL' >> /etc/sudoers
 
 usermod --password $(openssl passwd -6 $2) root
 usermod --password $(openssl passwd -6 $2) $1
@@ -60,31 +60,10 @@ apt install ansible -y
 #apt install puppet -y
 #apt install puppetserver -y
 
-
-
-#apt install postgresql -y
-
-#su - postgres -c 'psql --command "CREATE USER zabbix WITH PASSWORD '\'$3\'';"'
-
-#sudo -u postgres createdb -O zabbix zabbix
-
-#
-
-#wget $5
-
-#dpkg -i zabbix-release*.deb
-
-#apt update
-
-#apt install zabbix-server-pgsql zabbix-frontend-php php7.3-pgsql zabbix-apache-conf zabbix-sql-scripts zabbix-agent nano -y
-
-#zcat /usr/share/doc/zabbix-sql-scripts/postgresql/create.sql.gz | sudo -u zabbix psql zabbix
-
-#sed -i 's/# DBPassword=/DBPassword='$3'/g' /etc/zabbix/zabbix_server.conf
-
-#sudo systemctl restart zabbix-server zabbix-agent apache2
-#sudo systemctl enable zabbix-server zabbix-agent apache2
-
 echo "*******************************************************************************"
 echo "********************************* END *****************************************"
 echo "*******************************************************************************"
+echo "jenkins initial password"
+cat /var/lib/jenkins/secrets/initialAdminPassword
+echo "jenkins url"
+echo 'http://'$5':8080'
---
+++ bootstrap/tinc/tincgw/tincgw.sh
@@ -0,0 +1,28 @@
+# repo override
+#kz
+#sed -i 's/us.archive.ubuntu.com/mirror.hoster.kz/g' /etc/apt/sources.list
+#ru
+sed -i 's/us.archive.ubuntu.com/mirror.linux-ia64.org/g' /etc/apt/sources.list
+
+useradd $1 -s /bin/bash -d /home/test
+mkdir /home/test
+chown -R test:test /home/test
+echo ''$1'    ALL=(ALL:ALL) ALL' >> /etc/sudoers
+
+usermod --password $(openssl passwd -6 $2) root
+usermod --password $(openssl passwd -6 $2) $1
+
+
+apt update
+apt install htop
+if [ $3 == "true" ]; then apt upgrade -y; else echo '$3'=$3; fi
+
+rm -Rf /etc/hosts
+
+echo "127.0.0.1	localhost.localdomain	localhost" >> /etc/hosts
+echo "$5	$4.localdomain	$4" >> /etc/hosts
+
+echo "*******************************************************************************"
+echo "********************************* END *****************************************"
+echo "*******************************************************************************"
+
