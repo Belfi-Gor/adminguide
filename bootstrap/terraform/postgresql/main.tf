c9b99e6bc0ffc8941d744951316fdc9f09a698cc
Belfigor <belfi.dev@gmail.com>
2022-10-22 20:07:13 +0600

    Косметические правки

--- bootstrap/terraform/postgresql/main.tf
+++ bootstrap/terraform/postgresql/main.tf
@@ -9,11 +9,11 @@ terraform {
 
 provider "proxmox" {
   # url is the hostname (FQDN if you have one) for the proxmox host you'd like to connect to to issue the commands. my proxmox host is 'prox-1u'. Add /api2/json at the end for the API
-  pm_api_url = var.pm_api_url
+  pm_api_url = "https://192.168.10.230:8006/api2/json"
   # api token id is in the form of: <username>@pam!<tokenId>
-  pm_api_token_id = var.pm_api_token_id
+  pm_api_token_id = "root@pam!test"
   # this is the full secret wrapped in quotes. don't worry, I've already deleted this from my proxmox cluster by the time you read this post
-  pm_api_token_secret = var.pm_api_token_secret
+  pm_api_token_secret = "2382afbf-e3b1-4c4a-b541-367fb5db8a67"
   # leave tls_insecure set to true unless you have your proxmox SSL certificate situation fully sorted out (if you do, you will know)
   pm_tls_insecure = true
 }
