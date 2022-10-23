c14d6ae4b61626438e025062f6baf15803be4980
Belfigor <belfi.dev@gmail.com>
2022-10-23 23:33:30 +0600

    Vagrant Ubuntu VMs config with zabbix-agent installation & configuration for auto-discovery

---
+++ bootstrap/vagrant/ubuntu+zabbix-agent/zabbix-agent.sh
@@ -0,0 +1,39 @@
+# repo override
+#kz
+sed -i 's/us.archive.ubuntu.com/mirror.hoster.kz/g' /etc/apt/sources.list
+#ru
+#sed -i 's/us.archive.ubuntu.com/mirror.linux-ia64.org/g' /etc/apt/sources.list
+
+useradd $1 -s /bin/bash -d /home/test
+mkdir /home/test
+chown -R test:test /home/test
+echo ''$1'    ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers
+
+usermod --password $(openssl passwd -6 $2) root
+usermod --password $(openssl passwd -6 $2) $1
+
+
+apt update
+apt install htop -y
+
+if [ $3 == "true" ]; then apt upgrade -y; else echo '$3'=$3; fi
+
+rm -Rf /etc/hosts
+
+echo "127.0.0.1	localhost.localdomain	localhost" >> /etc/hosts
+echo "$5	$4.localdomain	$4" >> /etc/hosts
+
+echo "*******************************************************************************"
+echo "************************** INSTALLING ZABBIX-AGENT ****************************"
+echo "*******************************************************************************"
+wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-4%2Bdebian11_all.deb
+dpkg -i zabbix-release_6.0-4+debian11_all.deb
+apt update 
+apt install zabbix-agent -y
+sed -i 's/Server=/Server=$6/g' /etc/apt/sources.list
+systemctl restart zabbix-agent
+systemctl enable zabbix-agent
+
+echo "*******************************************************************************"
+echo "********************************* END *****************************************"
+echo "*******************************************************************************"
