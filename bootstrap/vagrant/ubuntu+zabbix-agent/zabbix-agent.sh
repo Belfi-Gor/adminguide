7869ca1e56dbed7568707b955ecc6d46d9dd29fa
Belfigor <belfi.dev@gmail.com>
2022-10-24 14:55:02 +0600

    Example: Ubuntu with preconfigured zabbix agent vagrant generation

--- bootstrap/vagrant/ubuntu+zabbix-agent/zabbix-agent.sh
+++ bootstrap/vagrant/ubuntu+zabbix-agent/zabbix-agent.sh
@@ -27,8 +27,26 @@ dpkg -i zabbix-release_6.0-4+debian11_all.deb
 apt update 
 apt install zabbix-agent -y
 sed -i "s/Server=127.0.0.1/Server=$6/g" /etc/zabbix/zabbix_agentd.conf
+echo 'UserParameter=custom_echo[*],echo $1' >> /etc/zabbix/zabbix_agentd.conf
+echo 'UserParameter=my_script[*], python3 /etc/zabbix/test_python_script.py $1 $2' > /etc/zabbix/zabbix_agentd.d/test_user_parameter.conf
 systemctl restart zabbix-agent
 systemctl enable zabbix-agent
+
+if [[ ! -f /etc/zabbix/test_python_script.py ]]
+then
+    echo 'import sys' >> /etc/zabbix/test_python_script.py
+    echo 'import os' >> /etc/zabbix/test_python_script.py
+    echo 'import re' >> /etc/zabbix/test_python_script.py
+    echo 'if (sys.argv[1] == "-ping"): # Если -ping' >> /etc/zabbix/test_python_script.py
+    echo '        result=os.popen("ping -c 1 " + sys.argv[2]).read() # Делаем пинг по заданному адресу' >> /etc/zabbix/test_python_script.py
+    echo '        result=re.findall(r"time=(.*) ms", result) # Выдёргиваем из результата время' >> /etc/zabbix/test_python_script.py
+    echo '        print(result[0]) # Выводим результат в консоль' >> /etc/zabbix/test_python_script.py
+    echo 'elif (sys.argv[1] == "-simple_print"): # Если simple_print ' >> /etc/zabbix/test_python_script.py
+    echo '        print(sys.argv[2]) # Выводим в консоль содержимое sys.arvg[2]' >> /etc/zabbix/test_python_script.py
+    echo 'else: # Во всех остальных случаях' >> /etc/zabbix/test_python_script.py
+    echo '        print(f"unknown input: {sys.argv[1]}") # Выводим непонятый запрос в консоль.' >> /etc/zabbix/test_python_script.py
+fi
+
 
 echo "*******************************************************************************"
 echo "********************************* END *****************************************"
