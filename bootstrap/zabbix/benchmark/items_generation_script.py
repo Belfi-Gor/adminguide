1ae0c7044bb2d4e024b44eefb17537e6cb8ef433
Belfigor <belfi.dev@gmail.com>
2022-12-10 23:05:50 +0600

    test_value counter adjusted

--- bootstrap/zabbix/benchmark/items_generation_script.py
+++ bootstrap/zabbix/benchmark/items_generation_script.py
@@ -3,7 +3,7 @@ import sys
 if (sys.argv[1] == '-generate_items'):
     result = []
     for i in range(int(sys.argv[2])):
-        result.append({"{#TESTITEM}": (f"test_value{i}")})
+        result.append({"{#TESTITEM}": (f"test_value{i+1}")})
     
     print(json.dumps(result))
 else: # In other cases
