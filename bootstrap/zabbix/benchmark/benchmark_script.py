7e2c157d2583a860045b440c06bfccbaa7c95e3f
Belfigor <SagutdinovASh@vseinstrumenti.ru>
2022-12-10 22:08:50 +0600

    benchmark script initial commit

---
+++ bootstrap/zabbix/benchmark/benchmark_script.py
@@ -0,0 +1,11 @@
+import sys
+import os
+import re
+if (sys.argv[1] == '-ping'): # Если -ping
+        result=os.popen("ping -c 1 " + sys.argv[2]).read() # Делаем пинг по заданному адресу
+        result=re.findall(r"time=(.*) ms", result) # Выдёргиваем из результата время
+        print(result[0]) # Выводим результат в консоль
+elif (sys.argv[1] == '-simple_print'): # Если simple_print 
+        print(sys.argv[2]) # Выводим в консоль содержимое sys.arvg[2]
+else: # Во всех остальных случаях
+        print(f"unknown input: {sys.argv[1]}") # Выводим непонятый запрос в консоль.
--- bootstrap/zabbix/benchmark/items_generation_script.py
+++ bootstrap/zabbix/benchmark/items_generation_script.py
@@ -1,9 +1,3 @@
-# import sys
-# if (sys.argv[1] == 'generate_items'):
-
-# else: # Во всех остальных случаях
-#         print(f"unknown input: {sys.argv[1]}") #Выводим непонятый запрос в консоль.
-
 import json
 result = []
 result.append({"{#test_item}":"test1","{#test_item}":"test2"})
