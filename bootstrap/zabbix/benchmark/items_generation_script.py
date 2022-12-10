bb11c87e17f381bffef8cc048c4aba38c642f297
Belfigor <SagutdinovASh@vseinstrumenti.ru>
2022-12-10 21:37:10 +0600

    test array

---
+++ bootstrap/zabbix/benchmark/items_generation_script.py
@@ -0,0 +1,11 @@
+# import sys
+# if (sys.argv[1] == 'generate_items'):
+
+# else: # Во всех остальных случаях
+#         print(f"unknown input: {sys.argv[1]}") #Выводим непонятый запрос в консоль.
+
+
+result = []
+result.append({"test1":1,"test2":2})
+result.append({"test3":3,"test4":4})
+print(result)
