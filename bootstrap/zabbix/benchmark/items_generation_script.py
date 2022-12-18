eb5c435f7d7f5d903f26e56c2f9a1b5008249dcb
Belfigor <belfi.dev@gmail.com>
2022-12-18 17:13:14 +0100

    minor fixes

--- bootstrap/zabbix/benchmark/items_generation_script.py
+++ bootstrap/zabbix/benchmark/items_generation_script.py
@@ -30,7 +30,7 @@ if (sys.argv[1] == '-generate_items'):
     result = []
     for i in range(int(sys.argv[2])):
         #if (sys.argv[3] == 'random_text_value'):
-        row = {"{#ITEMNAME}": f"{sys.argv[3]}{i+1}"}
+        row = {"{#"+f"{sys.argv[3]}{i+1}"+"_ITEMNAME}": f"{sys.argv[3]}{i+1}"}
         row["{#ITEMVALUE}"] = sys.argv[3]
         row["{#ITEMNUMBER}"] = i+1
         result.append(row)
